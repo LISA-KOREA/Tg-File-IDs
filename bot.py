4aed08a6d49cdbd9baa5ab02d23b1dfd13052994
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-04-19 12:07:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,12 +31,16 @@ async def handle_new_user_text(bot, message: Message):
     new_user_info = {
         "id": message.from_user.id,
 
+        
         "is_bot": message.from_user.is_bot,
 
+        
         "first_name": message.from_user.first_name,
 
+        
         "username": message.from_user.username,
 
+        
         "language_code": message.from_user.language_code
     }
     chat_info = {
